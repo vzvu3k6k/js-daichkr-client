cc68b59a5927412a70c99d747ab9e02435b10ff8
vzvu3k6k <vzvu3k6k@gmail.com>
2016-01-10 23:57:56 +0900

    Add tests for DaichkrClient#loginWithHatenaId, #getCsrfToken

---
+++ test/test.js
@@ -0,0 +1,29 @@
+import DaichkrClient from '../src/';
+import secret from './secret.json';
+
+function shouldFail(promise) {
+  return promise.then(() => Promise.reject('Should fail'), () => Promise.resolve());
+}
+
+describe('DaichkrClient', function () {
+  this.timeout(10000);
+  let loggedInClient;
+
+  describe('#loginWithHatenaId', function () {
+    it('should success with correct ID', function () {
+      loggedInClient = new DaichkrClient();
+      return loggedInClient.loginWithHatenaId(secret.hatenaId.username, secret.hatenaId.password);
+    });
+
+    it('should fail with wrong ID', function () {
+      const client = new DaichkrClient();
+      return shouldFail(client.loginWithHatenaId('!', '!'));
+    });
+  });
+
+  describe('#getCsrfToken', function () {
+    it('should success if already logged in', function () {
+      return loggedInClient.getCsrfToken();
+    });
+  });
+});

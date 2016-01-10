f9f7bf84b5d47abdfc836472d6b74709f63cf33a
vzvu3k6k <vzvu3k6k@gmail.com>
2016-01-11 00:15:56 +0900

    Add tests for Antenna#create, #delete
    Move timeout setting into mocha.opts

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
 /* eslint func-names:0 */
+import Antenna from '../src/antenna';
 import DaichkrClient from '../src/';
 import secret from './secret.json';
 
@@ -7,10 +8,9 @@ function shouldFail(promise) {
   return promise.then(() => Promise.reject('Should fail'), () => Promise.resolve());
 }
 
-describe('DaichkrClient', function () {
-  this.timeout(10000);
-  let loggedInClient;
+let loggedInClient;
 
+describe('DaichkrClient', function () {
   describe('#loginWithHatenaId', function () {
     it('should success with correct ID', function () {
       loggedInClient = new DaichkrClient();
@@ -26,6 +26,27 @@ describe('DaichkrClient', function () {
   describe('#getCsrfToken', function () {
     it('should success if already logged in', function () {
       return loggedInClient.getCsrfToken();
+    });
+  });
+});
+
+describe('Antenna', function () {
+  let tempAntenna;
+
+  describe('#create', function () {
+    it('should success', function () {
+      tempAntenna = new Antenna(loggedInClient, {
+        name: 'js-daichkr-client test',
+        description: 'test',
+        permission: 'secret',
+      });
+      return tempAntenna.create();
+    });
+  });
+
+  describe('#delete', function () {
+    it('should success', function () {
+      return tempAntenna.delete();
     });
   });
 });

0869bc3e3ce8027e14e975d588584fab8d45dfc5
vzvu3k6k <vzvu3k6k@gmail.com>
2016-11-25 21:38:18 +0900

    Make an cookiejar file when it doesn't exists
    to avoid an error

--- bin/bake_test_cookie.js
+++ bin/bake_test_cookie.js
@@ -1,11 +1,15 @@
 /* eslint import/no-extraneous-dependencies: ["error", { "devDependencies": true }] */
 import ToughCookieFilestore from 'tough-cookie-filestore';
+import fs from 'fs';
 import path from 'path';
 import DaichkrClient from '../src/';
 import hatenaId from '../test/secrets/hatenaId.json';
 
-const jar = new ToughCookieFilestore(path.resolve(__dirname, '../test/secrets/logined.jar'));
-
+const jarPath = path.resolve(__dirname, '../test/secrets/logined.jar');
+if (!fs.existsSync(jarPath)) {
+  fs.writeFileSync(jarPath, '');
+}
+const jar = new ToughCookieFilestore(jarPath);
 const client = new DaichkrClient({ jar });
 client.loginWithHatenaId(hatenaId.username, hatenaId.password)
   .then(
